d43d0bee052a0a2a94d42a3de602d80e94816c1d
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-08 00:12:53 -0500

    change localhost to 127.0.0.1

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 //const devLink = "http://localhost:3000/"
 const env = process.env.NODE_ENV;  //development or production
 
-export const base_url = env == "development" ? "http://localhost:3000" : "http://localhost:3000";
+export const base_url = process.env.BASE_URL;
 export const STRIPE_STATUS_COMPLETE = "complete";
